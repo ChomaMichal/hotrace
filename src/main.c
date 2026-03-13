e5ab071f47ffe1ecf83599347b806cca16e12e58
Michal Choma <michalkk04@gmail.com>
2026-03-13 21:36:30 +0100

    fuck you rading pushing to main

--- src/main.c
+++ src/main.c
@@ -11,102 +11,93 @@
 /* ************************************************************************** */
 
 #include "../inc/hotrace.h"
+#include <linux/limits.h>
 
+void read_all(t_mem_arena *arena) {
+  size_t i;
+  char *buff;
 
-static bool	read_chunks(t_buffer *buf)
-{
-	buf->cap = 65536;  //64KB
-	buf->used = 0;
-	buf->string = ft_calloc(1, buf->cap);
-	if (!buf->string)
-		return (false);
-	while (1)
-	{
-		buf->bytes = read(0, buf->string + buf->used, buf->cap - buf->used);
-		if (buf->bytes <= 0)
-		{
-			if (buf->bytes < 0)
-				return (false);
-			break;
-		}
-		buf->used = buf->used + buf->bytes;
-		if (buf->used == buf->cap)
-		{
-			buf->cap *= 2;
-			buf->string = realloc(buf->string, buf->cap); ///realloc self implementen with calloc
-			if (!buf->string)
-				return (false);
-		}
-	}
-	return (true);
+  i = 0;
+  while (1) {
+    buff = arena_push(arena, 64 * KIB);
+    i = read(1, buff, 64 * KIB);
+    if (i != 64 * KIB)
+      break;
+  }
+  if (i == -1) {
+    exit(1); // error handle
+  }
+  buff[i] = 0;
+  arena_pop_to(arena, (t_u64)buff + i);
 }
 
-int	main(int ac, char **av)
-{
-	(void) av;
-	if (ac != 1)
-		return (0);
+char *get_next_element(t_mem_arena *arena) {
+  static char *last = NULL;
+  char *this;
 
-	t_data data;
-	t_mem_arena		*arena;
+  if (last == NULL) {
+    last = (void *)arena + sizeof(t_mem_arena);
+    return (last);
+  }
+  this = last;
+  while (*this != '\n')
+    this++;
+  if (this == last)
+    return (NULL);
+  this++;
+  if (*this == 0)
+    return (NULL);
+  last = this;
+  return (this);
+}
 
-	arena = arena_create(8 * GIB);
-	if (arena == NULL)
-	{
-		return (write(2, "Arena creation failed", 1), 22);
-	}
+int main(int ac, char **av) {
+  t_mem_arena *arena;
 
-	// READ STDIN
-	if (!read_chunks(&data.buf))
-		return (ERROR);
-	if (VERBOSE)
-		printf("%s", data.buf.string);
+  arena = arena_create(8 * GIB);
+  if (arena == NULL) {
+    return (write(2, "Arena creation failed", 1), 22);
+  }
 
+  // READ STDIN
+  read_all(arena, )
 
+      /* - SPLIT INPUT IN 2 PHASES:
+        1) key/value pairs
+        2) search queries
+      */
 
-	/* - SPLIT INPUT IN 2 PHASES:
-	  1) key/value pairs
-	  2) search queries
-	*/
+      if (!pair_key_value(&data)) return (ERROR);
 
-	if (!pair_key_value(&data))
-		return (ERROR);
+  /*
+  PARSING
+  - READ STDIN 						check
+  - SPLIT INPUT IN 2 PHASES:			WIP
+    1) key/value pairs
+    2) search queries
+  - DETECT EMPTY LINE AS SEPARATOR
 
+  BUILD HASH TABLE
+  - HASH KEY WITH ALGORITHM djb2, FNV-1a, sdbm
+  - CALC INDEX IN TABLE
+  - HANDLE COLLISIONS
+  - SAVE KEY & VALUE
 
+  SEARCH
+  - HASH SEARCHWORD
+  - CALC START INDEX
+  - IF SLOT KEY == NULL -> NOT FOUND
+  - IF SLOT KEY MATCHES -> PRINT VALUE
+  - IF SLOT KEY EXISTS BUT NO MATCH -> LINEAR PROBING
+  - STOP WHEN:
+    - MATCH FOUND
+    - NULL SLOT FOUND
+    - OR YOU RETURNED TO START / TABLE FULL SAFETY
 
+  OUTPUT
+  - PRINT VALUE + '\n'
+  - ELSE PRINT "keyword Not found.\n"
+  */
 
-
-
-
-	/*
-	PARSING
-	- READ STDIN 						check
-	- SPLIT INPUT IN 2 PHASES:			WIP
-	  1) key/value pairs
-	  2) search queries
-	- DETECT EMPTY LINE AS SEPARATOR
-
-	BUILD HASH TABLE
-	- HASH KEY WITH ALGORITHM djb2, FNV-1a, sdbm
-	- CALC INDEX IN TABLE
-	- HANDLE COLLISIONS
-	- SAVE KEY & VALUE
-
-	SEARCH
-	- HASH SEARCHWORD
-	- CALC START INDEX
-	- IF SLOT KEY == NULL -> NOT FOUND
-	- IF SLOT KEY MATCHES -> PRINT VALUE
-	- IF SLOT KEY EXISTS BUT NO MATCH -> LINEAR PROBING
-	- STOP WHEN:
-	  - MATCH FOUND
-	  - NULL SLOT FOUND
-	  - OR YOU RETURNED TO START / TABLE FULL SAFETY
-
-	OUTPUT
-	- PRINT VALUE + '\n'
-	- ELSE PRINT "keyword Not found.\n"
-	*/
-
-	return (0);
+  return (0);
 }
